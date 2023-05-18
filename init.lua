70ecc34d036f7439613debf96ad83661ffcdc0b6
kanavaar <uju8765@gmail.com>
2023-05-18 11:29:41 +0200

    use ferafox colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "edge",
+  colorscheme = "carbonfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -14,4 +14,28 @@ return {
     "nyoom-engineering/oxocarbon.nvim",
     lazy = false,
   },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    init = function()
+      require("nightfox").setup({
+        options = {
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+          },
+        },
+        palettes = {
+          terafox = {
+            bg1 = "#080808",
+            fg1 = "#F5F5F5",
+          },
+          carbonfox = {
+            bg1 = "#080808",
+            fg1 = "#F5F5F5",
+          },
+        },
+      })
+    end,
+  },
 }
